8446c0854ce896ef008f2b56cc191bd32bea15d4
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-02-16 20:59:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-//node server 
-const io= require('socket.io') (8001 , {
-    cors:{
-        origin: "*"
-    }
-});
+const express = require('express'); // You might already have this
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 
 const users = {};
 
-io.on('connection', socket =>{
-    socket.on('new-user-joined', name =>{
-        console.log("New user", name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+io.on('connection', socket => {
+  socket.on('new-user-joined', name => {
+    console.log("New user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
 
+  socket.on('send', message => {
+    socket.broadcast.emit(`receive`, {
+      message,
+      name: users[socket.id],
     });
+  });
 
-    socket.on('send', message =>{
-        socket.broadcast.emit(`receive`, {message: message, name: users[socket.id]})
-    });
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit(`left`, users[socket.id])
-        delete users[socket.id];
-    });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit(`left`, users[socket.id]);
+    delete users[socket.id];
+  });
 });
+
+// Listen on the Port from environment variable or default to 8000
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+//node server 
+
+
+
+
+
+
+
+
+
+
